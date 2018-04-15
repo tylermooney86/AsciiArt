3ab81a4103f63da8300d26895e84f4458035010b
tylermooney86 <tyler.mooney@colorado.edu>
2018-04-15 17:42:01 -0600

    adding comments

--- ascii.py
+++ ascii.py
@@ -1,15 +1,52 @@
-#
+'''
+# ###################
 # Ascii art generator
+# ###################
+This program allows the user to create ascii art from an image.
+It uses a simple menu system to control the parameters of
+the generated image.
 
+##### Menus #####
+Select Image:
+    prints a list of images stored in images folder
+Generate Ascii Art:
+    Two output modes:
+        Monochrome: uses two char to render image
+        Grayscale: uses many char to render image
+Settings:
+    Character Lists:
+        Change Character List:
+            Customize character lists used to render ascii art
+            Do not separate characters
+                I recommend using smaller character sets for grayscale set and dont for get the space(' ')
+            Monochrome only takes two characters in the order they are entered
+            Grayscale takes many characters and orders them by density of the character
+        Invert Character List:
+            Reverses the order of the characters in character list
+    Dimensions:
+        Change dimensions of output
+            input should be in the form: width,height (ie: 80,40 or characters,lines)
+            entering an incorrectly formatted value will cause program to use input image's dimensions
+    Brightness/Contrast:
+        Takes float values:
+            1.0 = no brightness/contrast Change
+            0.0 = black image
+            >1.0 = high contrast/Brightness
+            not a number = None
+View Generated Images:
+    Prints selected text file to console
+
+'''
 from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 from os import listdir
 import sys
 import string
 import collections
-
+'''
 # ################
 # Global Variables
 # ################
+'''
 image_dimensions = (80,40)
 font = ImageFont.load_default()
 image_dir = 'Images/'
@@ -17,8 +54,6 @@ output_dir = 'AsciiArt/'
 selected_image = None
 contrast = None
 brightness = None
-
-#TODO use ImageEnhance to create contrast/brightness settings for more control
 
 '''
 # ############
